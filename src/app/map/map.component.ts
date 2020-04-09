a66622c8d721cdfdb6e940a0e57499d7c452bd37
Forestator <forestator@gmail.com>
2020-04-09 23:25:40 +0200

    Correctif pour l'affichage des marqueurs

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component} from '@angular/core';
 import * as L from 'leaflet';
 import {CRS, Map, MapOptions} from 'leaflet';
+import {MarkerOptions} from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -28,11 +29,25 @@ export class MapComponent implements AfterViewInit {
     L.imageOverlay('assets/map/carteNO.jpg', bounds).addTo(this.map);
 
     this.map.fitBounds(bounds);
-    this.map.on('click', (ev) =>  this.clickEvent(ev));
+    this.map.on('click', (ev) => this.clickEvent(ev));
   }
 
   private clickEvent(ev: any) {
-    const marker = L.marker([ev.latlng.lat, ev.latlng.lng]).addTo(this.map);
+    console.log(ev);
+
+    const icon = L.icon({
+      iconUrl: 'assets/marker-icon.png',
+
+      iconSize: [20, 30], // size of the icon
+      shadowSize: [20, 30], // size of the shadow
+      iconAnchor: [20, 30], // point of the icon which will correspond to marker's location
+      popupAnchor: [-10, -30] // point from which the popup should open relative to the iconAnchor
+    });
+
+    const markerOptions: MarkerOptions = {};
+    markerOptions.icon = icon;
+
+    const marker = L.marker([ev.latlng.lat, ev.latlng.lng], markerOptions).addTo(this.map);
     marker.bindPopup('On peut marquer des trucs !');
   }
 }
